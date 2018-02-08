2c3b3be7c38e2f1d303150e51213f75c3c7f9fd1
Julio <32122749+jcuatro@users.noreply.github.com>
2018-02-08 10:28:06 +0100

    Proyecto 6 adjunto al 5

---
+++ actualizarUsuario.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="actualizar.css">
+    <title>Registro</title>
+  </head>
+  <body>
+    <fieldset>
+    <legend><h1>Actualizar</h1></legend>
+    <p>ID: <br> <input type="text" name="" value=""></p>
+    <p>Usuario: <br> <input type="text" name="" value=""></p>
+    <p>Nombre: <br> <input type="text" name="" value=""></p>
+    <p>Apellidos: <br> <input type="text" name="" value=""></p>
+    <p>Fecha de nacimiento: <br> <input type="text" name="" value=""></p>
+    <p>Curso: <br> <input type="text" name="" value=""></p>
+    </fieldset>
+  </body>
+</html>
